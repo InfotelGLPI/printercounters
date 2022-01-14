ecb4e45d00bff6a669f7e0eee85c951589063f55
tsmr <thetsmr@gmail.com>
2022-01-14 09:26:03 +0100

    Update headers & int unsigned

--- install/sql/empty-2.0.0.sql
+++ install/sql/empty-2.0.0.sql
@@ -4,22 +4,22 @@
 -- --------------------------------------------------------
 DROP TABLE IF EXISTS `glpi_plugin_printercounters_configs`;
 CREATE TABLE IF NOT EXISTS `glpi_plugin_printercounters_configs` (
-   `id` int(11) NOT NULL auto_increment,
-   `nb_errors_ticket` int(11) NOT NULL default '10',
-   `nb_errors_delay_ticket` int(11) NOT NULL default '864000',
-   `no_record_delay_ticket` int(11) NOT NULL default '864000',
+   `id` int unsigned NOT NULL auto_increment,
+   `nb_errors_ticket` int unsigned NOT NULL default '10',
+   `nb_errors_delay_ticket` int unsigned NOT NULL default '864000',
+   `no_record_delay_ticket` int unsigned NOT NULL default '864000',
    `items_status` longtext COLLATE utf8mb4_unicode_ci,
-   `tickets_category` int(11) NOT NULL default '0',
+   `tickets_category` int unsigned NOT NULL default '0',
    `tickets_content` text COLLATE utf8mb4_unicode_ci,
    `add_item_group` char(1) collate utf8mb4_unicode_ci default NULL,
    `add_item_user` char(1) collate utf8mb4_unicode_ci default NULL,
-   `disable_autosearch` tinyint(1) NOT NULL default '0',
-   `set_first_record` tinyint(1) NOT NULL DEFAULT '0',
-   `enable_toner_alert` tinyint(1) NOT NULL DEFAULT '0',
-   `toner_alert_repeat` int(11) NOT NULL DEFAULT '0',
-   `toner_treshold` int(11) NOT NULL DEFAULT '0',
-   `max_error_counter` int(11) NOT NULL DEFAULT '3',
-   `enable_error_handler` int(11) NOT NULL DEFAULT '0',
+   `disable_autosearch` tinyint NOT NULL default '0',
+   `set_first_record` tinyint NOT NULL DEFAULT '0',
+   `enable_toner_alert` tinyint NOT NULL DEFAULT '0',
+   `toner_alert_repeat` int unsigned NOT NULL DEFAULT '0',
+   `toner_treshold` int unsigned NOT NULL DEFAULT '0',
+   `max_error_counter` int unsigned NOT NULL DEFAULT '3',
+   `enable_error_handler` int unsigned NOT NULL DEFAULT '0',
    PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci ROW_FORMAT=DYNAMIC;
 
@@ -30,10 +30,10 @@ INSERT INTO `glpi_plugin_printercounters_configs` VALUES ('1', '10', '864000', '
 -- --------------------------------------------------------
 DROP TABLE IF EXISTS `glpi_plugin_printercounters_countertypes`;
 CREATE TABLE IF NOT EXISTS `glpi_plugin_printercounters_countertypes` (
-   `id` int(11) NOT NULL auto_increment,
+   `id` int unsigned NOT NULL auto_increment,
    `name` varchar(255) default NULL,
-   `entities_id` int(11) NOT NULL default '0',
-   `is_recursive` int(11) NOT NULL default '0',
+   `entities_id` int unsigned NOT NULL default '0',
+   `is_recursive` int unsigned NOT NULL default '0',
    `comment` text COLLATE utf8mb4_unicode_ci,
    PRIMARY KEY (`id`),
    KEY `entities_id` (`entities_id`)
@@ -44,11 +44,11 @@ CREATE TABLE IF NOT EXISTS `glpi_plugin_printercounters_countertypes` (
 -- --------------------------------------------------------
 DROP TABLE IF EXISTS `glpi_plugin_printercounters_countertypes_recordmodels`;
 CREATE TABLE IF NOT EXISTS `glpi_plugin_printercounters_countertypes_recordmodels` (
-   `id` int(11) NOT NULL auto_increment,
+   `id` int unsigned NOT NULL auto_increment,
    `oid` varchar(255) default NULL,
-   `oid_type` int(11) NOT NULL default '0',
-   `plugin_printercounters_recordmodels_id` int(11) NOT NULL default '0',
-   `plugin_printercounters_countertypes_id` int(11) NOT NULL default '0',
+   `oid_type` int unsigned NOT NULL default '0',
+   `plugin_printercounters_recordmodels_id` int unsigned NOT NULL default '0',
+   `plugin_printercounters_countertypes_id` int unsigned NOT NULL default '0',
    PRIMARY KEY (`id`),
    UNIQUE KEY `unicity` (`plugin_printercounters_recordmodels_id`, `plugin_printercounters_countertypes_id`),
    KEY `oid_type` (`oid_type`),
@@ -61,16 +61,16 @@ CREATE TABLE IF NOT EXISTS `glpi_plugin_printercounters_countertypes_recordmodel
 -- --------------------------------------------------------
 DROP TABLE IF EXISTS `glpi_plugin_printercounters_recordmodels`;
 CREATE TABLE IF NOT EXISTS `glpi_plugin_printercounters_recordmodels` (
-   `id` int(11) NOT NULL auto_increment,
+   `id` int unsigned NOT NULL auto_increment,
    `name` varchar(255) default NULL,
-   `entities_id` int(11) NOT NULL default '0',
-   `is_recursive` tinyint(1) NOT NULL default '0',
-   `mac_address_conformity` tinyint(1) NOT NULL default '0',
-   `sysdescr_conformity` tinyint(1) NOT NULL default '0',
-   `serial_conformity` tinyint(1) NOT NULL default '0',
-   `enable_toner_level` tinyint(1) NOT NULL DEFAULT '0',
-   `enable_printer_info` tinyint(1) NOT NULL DEFAULT '0',
-   `is_template` tinyint(1) NOT NULL default '0',
+   `entities_id` int unsigned NOT NULL default '0',
+   `is_recursive` tinyint NOT NULL default '0',
+   `mac_address_conformity` tinyint NOT NULL default '0',
+   `sysdescr_conformity` tinyint NOT NULL default '0',
+   `serial_conformity` tinyint NOT NULL default '0',
+   `enable_toner_level` tinyint NOT NULL DEFAULT '0',
+   `enable_printer_info` tinyint NOT NULL DEFAULT '0',
+   `is_template` tinyint NOT NULL default '0',
    `template_name` varchar(255) default NULL,
    `comment` text COLLATE utf8mb4_unicode_ci,
    PRIMARY KEY (`id`),
@@ -82,9 +82,9 @@ CREATE TABLE IF NOT EXISTS `glpi_plugin_printercounters_recordmodels` (
 -- --------------------------------------------------------
 DROP TABLE IF EXISTS `glpi_plugin_printercounters_sysdescrs`;
 CREATE TABLE IF NOT EXISTS `glpi_plugin_printercounters_sysdescrs` (
-   `id` int(11) NOT NULL auto_increment,
+   `id` int unsigned NOT NULL auto_increment,
    `sysdescr` varchar(255) default NULL,
-   `plugin_printercounters_recordmodels_id` int(11) NOT NULL default '0',
+   `plugin_printercounters_recordmodels_id` int unsigned NOT NULL default '0',
    PRIMARY KEY (`id`),
    KEY `plugin_printercounters_recordmodels_id` (`plugin_printercounters_recordmodels_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci ROW_FORMAT=DYNAMIC;
@@ -94,20 +94,20 @@ CREATE TABLE IF NOT EXISTS `glpi_plugin_printercounters_sysdescrs` (
 -- --------------------------------------------------------
 DROP TABLE IF EXISTS `glpi_plugin_printercounters_items_recordmodels`;
 CREATE TABLE IF NOT EXISTS `glpi_plugin_printercounters_items_recordmodels` (
-   `id` int(11) NOT NULL auto_increment,
-   `nb_retries` int(11) NOT NULL default '0',
-   `max_timeout` int(11) NOT NULL default '5',
-   `enable_automatic_record` tinyint(1) NOT NULL default '0',
-   `periodicity` int(11) NOT NULL default '86400',
-   `items_id` int(11) NOT NULL default '0',
+   `id` int unsigned NOT NULL auto_increment,
+   `nb_retries` int unsigned NOT NULL default '0',
+   `max_timeout` int unsigned NOT NULL default '5',
+   `enable_automatic_record` tinyint NOT NULL default '0',
+   `periodicity` int unsigned NOT NULL default '86400',
+   `items_id` int unsigned NOT NULL default '0',
    `itemtype` varchar(255) default NULL,
    `active_mutex` timestamp NULL DEFAULT NULL,
-   `process_id` int(11) NOT NULL default '0',
+   `process_id` int unsigned NOT NULL default '0',
    `global_tco` decimal(20,4) DEFAULT '0.0000',
-   `plugin_printercounters_recordmodels_id` int(11) NOT NULL default '0',
-   `plugin_printercounters_snmpauthentications_id` int(11) NOT NULL default '0',
-   `status` int(11) NOT NULL default '0',
-   `error_counter` int(11) NOT NULL default '0',
+   `plugin_printercounters_recordmodels_id` int unsigned NOT NULL default '0',
+   `plugin_printercounters_snmpauthentications_id` int unsigned NOT NULL default '0',
+   `status` int unsigned NOT NULL default '0',
+   `error_counter` int unsigned NOT NULL default '0',
    PRIMARY KEY (`id`),
    UNIQUE KEY `unicity` (`plugin_printercounters_recordmodels_id`,`items_id`, `itemtype`),
    KEY `plugin_printercounters_recordmodels_id` (`plugin_printercounters_recordmodels_id`),
@@ -121,10 +121,10 @@ CREATE TABLE IF NOT EXISTS `glpi_plugin_printercounters_items_recordmodels` (
 -- --------------------------------------------------------
 DROP TABLE IF EXISTS `glpi_plugin_printercounters_counters`;
 CREATE TABLE IF NOT EXISTS `glpi_plugin_printercounters_counters` (
-   `id` int(11) NOT NULL auto_increment,
-   `value` int(11) NOT NULL default '0',
-   `plugin_printercounters_countertypes_recordmodels_id` int(11) NOT NULL default '0',
-   `plugin_printercounters_records_id` int(11) NOT NULL default '0',
+   `id` int unsigned NOT NULL auto_increment,
+   `value` int unsigned NOT NULL default '0',
+   `plugin_printercounters_countertypes_recordmodels_id` int unsigned NOT NULL default '0',
+   `plugin_printercounters_records_id` int unsigned NOT NULL default '0',
    PRIMARY KEY (`id`),
    KEY `plugin_printercounters_countertypes_recordmodels_id` (`plugin_printercounters_countertypes_recordmodels_id`),
    KEY `plugin_printercounters_records_id` (`plugin_printercounters_records_id`)
@@ -135,15 +135,15 @@ CREATE TABLE IF NOT EXISTS `glpi_plugin_printercounters_counters` (
 -- --------------------------------------------------------
 DROP TABLE IF EXISTS `glpi_plugin_printercounters_records`;
 CREATE TABLE IF NOT EXISTS `glpi_plugin_printercounters_records` (
-   `id` int(11) NOT NULL auto_increment,
+   `id` int unsigned NOT NULL auto_increment,
    `date` timestamp NULL DEFAULT NULL,
-   `entities_id` int(11) NOT NULL default '0',
-   `result` int(11) NOT NULL default '0',
-   `state` int(11) NOT NULL default '0',
-   `record_type` int(11) NOT NULL default '0',
-   `locations_id` int(11) NOT NULL default '0',
-   `last_recordmodels_id` int(11) NOT NULL default '0',
-   `plugin_printercounters_items_recordmodels_id` int(11) NOT NULL default '0',
+   `entities_id` int unsigned NOT NULL default '0',
+   `result` int unsigned NOT NULL default '0',
+   `state` int unsigned NOT NULL default '0',
+   `record_type` int unsigned NOT NULL default '0',
+   `locations_id` int unsigned NOT NULL default '0',
+   `last_recordmodels_id` int unsigned NOT NULL default '0',
+   `plugin_printercounters_items_recordmodels_id` int unsigned NOT NULL default '0',
    PRIMARY KEY (`id`),
    KEY `plugin_printercounters_items_recordmodels_id` (`plugin_printercounters_items_recordmodels_id`),
    KEY `date` (`date`),
@@ -160,16 +160,16 @@ CREATE TABLE IF NOT EXISTS `glpi_plugin_printercounters_records` (
 -- --------------------------------------------------------
 DROP TABLE IF EXISTS `glpi_plugin_printercounters_snmpauthentications`;
 CREATE TABLE IF NOT EXISTS `glpi_plugin_printercounters_snmpauthentications` (
-   `id` int(11) NOT NULL auto_increment,
+   `id` int unsigned NOT NULL auto_increment,
    `name` varchar(255) default NULL,
-   `entities_id` int(11) NOT NULL default '0',
-   `is_recursive` tinyint(1) NOT NULL default '0',
-   `is_default` tinyint(1) NOT NULL default '0',
-   `version` int(11) NOT NULL default '0',
+   `entities_id` int unsigned NOT NULL default '0',
+   `is_recursive` tinyint NOT NULL default '0',
+   `is_default` tinyint NOT NULL default '0',
+   `version` int unsigned NOT NULL default '0',
    `community` varchar(255) NULL default 'public',
    `community_write` varchar(255) NULL default 'private',
-   `authentication_encrypt` int(11) NOT NULL default '0',
-   `data_encrypt` int(11) NOT NULL default '0',
+   `authentication_encrypt` int unsigned NOT NULL default '0',
+   `data_encrypt` int unsigned NOT NULL default '0',
    `user` varchar(255) default NULL,
    `authentication_password` varchar(255) default NULL,
    `data_password` varchar(255) default NULL,
@@ -183,10 +183,10 @@ CREATE TABLE IF NOT EXISTS `glpi_plugin_printercounters_snmpauthentications` (
 -- --------------------------------------------------------
 DROP TABLE IF EXISTS `glpi_plugin_printercounters_pagecosts`;
 CREATE TABLE IF NOT EXISTS `glpi_plugin_printercounters_pagecosts` (
-   `id` int(11) NOT NULL auto_increment,
+   `id` int unsigned NOT NULL auto_increment,
    `cost` decimal(6,5) NOT NULL default '0',
-   `plugin_printercounters_billingmodels_id` int(11) NOT NULL default '0',
-   `plugin_printercounters_countertypes_id` int(11) NOT NULL default '0',
+   `plugin_printercounters_billingmodels_id` int unsigned NOT NULL default '0',
+   `plugin_printercounters_countertypes_id` int unsigned NOT NULL default '0',
    PRIMARY KEY (`id`),
    KEY `plugin_printercounters_billingmodels_id` (`plugin_printercounters_billingmodels_id`),
    KEY `plugin_printercounters_countertypes_id` (`plugin_printercounters_countertypes_id`)
@@ -197,15 +197,15 @@ CREATE TABLE IF NOT EXISTS `glpi_plugin_printercounters_pagecosts` (
 -- --------------------------------------------------------
 DROP TABLE IF EXISTS `glpi_plugin_printercounters_billingmodels`;
 CREATE TABLE IF NOT EXISTS `glpi_plugin_printercounters_billingmodels` (
-   `id` int(11) NOT NULL auto_increment,
+   `id` int unsigned NOT NULL auto_increment,
    `name` varchar(255) default NULL,
-   `entities_id` int(11) NOT NULL default '0',
-   `is_recursive` tinyint(1) NOT NULL default '0',
+   `entities_id` int unsigned NOT NULL default '0',
+   `is_recursive` tinyint NOT NULL default '0',
    `application_date` timestamp NULL DEFAULT NULL,
-   `plugin_printercounters_recordmodels_id` int(11) NOT NULL default '0',
-   `contracts_id` int(11) NOT NULL default '0',
-   `budgets_id` int(11) NOT NULL default '0',
-   `suppliers_id` int(11) NOT NULL default '0',
+   `plugin_printercounters_recordmodels_id` int unsigned NOT NULL default '0',
+   `contracts_id` int unsigned NOT NULL default '0',
+   `budgets_id` int unsigned NOT NULL default '0',
+   `suppliers_id` int unsigned NOT NULL default '0',
    `comment` text COLLATE utf8mb4_unicode_ci,
    PRIMARY KEY (`id`),
    KEY `plugin_printercounters_recordmodels_id` (`plugin_printercounters_recordmodels_id`),
@@ -221,10 +221,10 @@ CREATE TABLE IF NOT EXISTS `glpi_plugin_printercounters_billingmodels` (
 -- --------------------------------------------------------
 DROP TABLE IF EXISTS `glpi_plugin_printercounters_items_billingmodels`;
 CREATE TABLE IF NOT EXISTS `glpi_plugin_printercounters_items_billingmodels` (
-   `id` int(11) NOT NULL auto_increment,
+   `id` int unsigned NOT NULL auto_increment,
    `itemtype` varchar(255) default NULL,
-   `items_id` int(11) NOT NULL default '0',
-   `plugin_printercounters_billingmodels_id` int(11) NOT NULL default '0',
+   `items_id` int unsigned NOT NULL default '0',
+   `plugin_printercounters_billingmodels_id` int unsigned NOT NULL default '0',
    PRIMARY KEY (`id`),
    KEY `plugin_printercounters_billingmodels_id` (`plugin_printercounters_billingmodels_id`),
    KEY `items_id` (`items_id`),
@@ -236,11 +236,11 @@ CREATE TABLE IF NOT EXISTS `glpi_plugin_printercounters_items_billingmodels` (
 -- --------------------------------------------------------
 DROP TABLE IF EXISTS `glpi_plugin_printercounters_budgets`;
 CREATE TABLE IF NOT EXISTS `glpi_plugin_printercounters_budgets` (
-   `id` int(11) NOT NULL auto_increment,
+   `id` int unsigned NOT NULL auto_increment,
    `name` varchar(255) default NULL,
-   `entities_id` int(11) default NULL,
-   `is_recursive` tinyint(1) NOT NULL default '0',
-   `amount` int(11) NOT NULL default '0',
+   `entities_id` int unsigned default NULL,
+   `is_recursive` tinyint NOT NULL default '0',
+   `amount` int unsigned NOT NULL default '0',
    `begin_date` timestamp NULL DEFAULT NULL,
    `end_date` timestamp NULL DEFAULT NULL,
    `comment` text COLLATE utf8mb4_unicode_ci,
@@ -255,12 +255,12 @@ CREATE TABLE IF NOT EXISTS `glpi_plugin_printercounters_budgets` (
 -- --------------------------------------------------------
 DROP TABLE IF EXISTS `glpi_plugin_printercounters_items_tickets`;
 CREATE TABLE IF NOT EXISTS `glpi_plugin_printercounters_items_tickets` (
-   `id` int(11) NOT NULL auto_increment,
+   `id` int unsigned NOT NULL auto_increment,
    `itemtype` varchar(255) default NULL,
-   `items_id` int(11) NOT NULL default '0',
-   `events_type` int(11) NOT NULL default '0',
+   `items_id` int unsigned NOT NULL default '0',
+   `events_type` int unsigned NOT NULL default '0',
    `date_mod` timestamp NULL DEFAULT NULL,
-   `tickets_id` int(11) NOT NULL default '0',
+   `tickets_id` int unsigned NOT NULL default '0',
    PRIMARY KEY (`id`),
    KEY `tickets_id` (`tickets_id`),
    KEY `items_id` (`items_id`),
@@ -272,12 +272,12 @@ CREATE TABLE IF NOT EXISTS `glpi_plugin_printercounters_items_tickets` (
 -- --------------------------------------------------------
 DROP TABLE IF EXISTS `glpi_plugin_printercounters_snmpsets`;
 CREATE TABLE IF NOT EXISTS `glpi_plugin_printercounters_snmpsets` (
-   `id` int(11) NOT NULL auto_increment,
-   `entities_id` int(11) NOT NULL default '0',
-   `is_recursive` tinyint(1) NOT NULL default '0',
-   `set_location` tinyint(1) NOT NULL default '1',
-   `set_contact` tinyint(1) NOT NULL default '1',
-   `set_name` tinyint(1) NOT NULL default '1',
+   `id` int unsigned NOT NULL auto_increment,
+   `entities_id` int unsigned NOT NULL default '0',
+   `is_recursive` tinyint NOT NULL default '0',
+   `set_location` tinyint NOT NULL default '1',
+   `set_contact` tinyint NOT NULL default '1',
+   `set_name` tinyint NOT NULL default '1',
    `contact` text COLLATE utf8mb4_unicode_ci,
    PRIMARY KEY (`id`),
    UNIQUE KEY `unicity` (`entities_id`),
@@ -289,12 +289,12 @@ CREATE TABLE IF NOT EXISTS `glpi_plugin_printercounters_snmpsets` (
 -- --------------------------------------------------------
 DROP TABLE IF EXISTS `glpi_plugin_printercounters_additionals_datas`;
 CREATE TABLE IF NOT EXISTS `glpi_plugin_printercounters_additionals_datas` (
-   `id` int(11) NOT NULL auto_increment,
+   `id` int unsigned NOT NULL auto_increment,
    `name` varchar(255) default NULL,
    `type` varchar(255) default NULL,
    `sub_type` varchar(255) default NULL,
    `value` varchar(255) default NULL,
-   `plugin_printercounters_items_recordmodels_id` int(11) NOT NULL default '0',
+   `plugin_printercounters_items_recordmodels_id` int unsigned NOT NULL default '0',
    PRIMARY KEY (`id`),
    UNIQUE KEY `unicity` (`plugin_printercounters_items_recordmodels_id`, `sub_type`),
    KEY `type` (`type`)
