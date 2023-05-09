75dc70b3275bf00ccf260164be3eb26d280d3627
kabassanov <kabassanov@users.noreply.github.com>
2023-05-09 11:07:31 +0200

    Passing null to parameter #... is deprecated

--- inc/search.class.php
+++ inc/search.class.php
@@ -919,7 +919,7 @@ class PluginPrintercountersSearch extends CommonDBTM
                 $search = ["/\&lt;/", "/\&gt;/"];
                 $replace = ["<", ">"];
                 $val = preg_replace($search, $replace, $val);
-                if (preg_match("/^\s*([<>])([=]*)[[:space:]]*([0-9\.]+)/", $val, $regs)) {
+                if ($avl && preg_match("/^\s*([<>])([=]*)[[:space:]]*([0-9\.]+)/", $val, $regs)) {
                     if ($nott) {
                         if ($regs[1] == '<') {
                             $regs[1] = '>';
